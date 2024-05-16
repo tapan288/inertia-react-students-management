0fcf006a642a05ec6d66d8465cdb30519aed1dfe
Tapan Sharma <tapansharma69@gmail.com>
2024-05-16 13:44:52 +0545

    display students data

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Inertia\Inertia;
+use App\Models\Student;
+use Illuminate\Http\Request;
+use App\Http\Resources\StudentResource;
+
+class StudentController extends Controller
+{
+    public function index()
+    {
+        $students = Student::all();
+
+        return Inertia::render('Student/Index', [
+            'students' => StudentResource::collection($students),
+        ]);
+    }
+}
---
+++ app/Http/Resources/StudentResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use App\Http\Resources\ClassResource;
+use App\Http\Resources\SectionResource;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class StudentResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'class' => ClassResource::make($this->class),
+            'section' => SectionResource::make($this->section),
+            'created_at' => $this->created_at->toFormattedDateString(),
+        ];
+    }
+}
