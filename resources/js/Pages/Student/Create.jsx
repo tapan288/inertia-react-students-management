91fe1a4d7ae8eaf4431e0d0338b4356b83865ed4
Tapan Sharma <tapansharma69@gmail.com>
2024-05-17 13:52:17 +0545

    implement validation and finalize record create flow

--- resources/js/Pages/Student/Create.jsx
+++ resources/js/Pages/Student/Create.jsx
@@ -1,5 +1,6 @@
+import InputError from "@/Components/InputError";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-import { Head, useForm } from "@inertiajs/react";
+import { Head, Link, useForm } from "@inertiajs/react";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
@@ -29,7 +30,7 @@ export default function Create({ auth, classes }) {
 
     function submit(e) {
         e.preventDefault();
-        // post("/login");
+        post(route("students.store"));
     }
     return (
         <AuthenticatedLayout
@@ -45,7 +46,7 @@ export default function Create({ auth, classes }) {
             <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
                 <div className="lg:grid lg:grid-cols-12 lg:gap-x-5">
                     <div className="space-y-6 sm:px-6 lg:px-0 lg:col-span-12">
-                        <form>
+                        <form onSubmit={submit}>
                             <div className="shadow sm:rounded-md sm:overflow-hidden">
                                 <div className="bg-white py-6 px-4 space-y-6 sm:p-6">
                                     <div>
@@ -76,11 +77,13 @@ export default function Create({ auth, classes }) {
                                                     )
                                                 }
                                                 id="name"
-                                                className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm @error text-red-900 focus:ring-red-500 focus:border-red-500 border-red-300 @enderror"
+                                                className={`mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm ${
+                                                    errors.name
+                                                        ? "text-red-900 focus:ring-red-500 focus:border-red-500 border-red-300"
+                                                        : ""
+                                                }`}
                                             />
-                                            <p className="text-red-500">
-                                                This field is required
-                                            </p>
+                                            <InputError message={errors.name} />
                                         </div>
 
                                         <div className="col-span-6 sm:col-span-3">
@@ -101,7 +104,14 @@ export default function Create({ auth, classes }) {
                                                 }
                                                 id="email"
                                                 autoComplete="email"
-                                                className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                                className={`mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm ${
+                                                    errors.email
+                                                        ? "text-red-900 focus:ring-red-500 focus:border-red-500 border-red-300"
+                                                        : ""
+                                                }`}
+                                            />
+                                            <InputError
+                                                message={errors.email}
                                             />
                                         </div>
 
@@ -121,7 +131,11 @@ export default function Create({ auth, classes }) {
                                                         e.target.value
                                                     )
                                                 }
-                                                className="mt-1 block w-full bg-white border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                                className={`mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm ${
+                                                    errors.class_id
+                                                        ? "text-red-900 focus:ring-red-500 focus:border-red-500 border-red-300"
+                                                        : ""
+                                                }`}
                                             >
                                                 <option value="">
                                                     Select a Class
@@ -137,6 +151,9 @@ export default function Create({ auth, classes }) {
                                                     );
                                                 })}
                                             </select>
+                                            <InputError
+                                                message={errors.class_id}
+                                            />
                                         </div>
 
                                         <div className="col-span-6 sm:col-span-3">
@@ -155,7 +172,11 @@ export default function Create({ auth, classes }) {
                                                         e.target.value
                                                     )
                                                 }
-                                                className="mt-1 block w-full bg-white border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                                className={`mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm ${
+                                                    errors.section_id
+                                                        ? "text-red-900 focus:ring-red-500 focus:border-red-500 border-red-300"
+                                                        : ""
+                                                }`}
                                             >
                                                 <option value="">
                                                     Select a Section
@@ -166,22 +187,24 @@ export default function Create({ auth, classes }) {
                                                             value={section.id}
                                                             key={section.id}
                                                         >
-                                                            {section.name} -{" "}
-                                                            {section.class_id}
+                                                            {section.name}
                                                         </option>
                                                     );
                                                 })}
                                             </select>
+                                            <InputError
+                                                message={errors.section_id}
+                                            />
                                         </div>
                                     </div>
                                 </div>
                                 <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                                    <a
-                                        href="#"
+                                    <Link
+                                        href={route("students.index")}
                                         className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mr-4"
                                     >
                                         Cancel
-                                    </a>
+                                    </Link>
                                     <button
                                         type="submit"
                                         className="bg-indigo-600 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
